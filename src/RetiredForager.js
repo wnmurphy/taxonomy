d51e9f12aa77fea7452e559889bb69be6bb26932
Neil Murphy <wnmurphy@gmail.com>
2016-01-19 08:26:02 -0800

    Cathup commit; floobits + git was not seeing changes to files.

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,14 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.color = 'grey';
+  this.job = 'gamble';
+  this.canFly = false;
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.gamble = ForagerBee.prototype.forage;
